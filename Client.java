3faeaca69c3a0322dc414f5d33d017bd9f0b6590
Patrick Sigourney <psigourney@gmail.com>
2018-07-14 14:57:24 -0500

    Created new repository for SW Testing project

---
+++ Client.java
@@ -0,0 +1,98 @@
+//Distributed Systems Fall 2017
+//Homework 1
+//Patrick Sigourney & Howie Benefiel
+
+import java.util.Scanner;
+import java.io.*;
+import java.net.*;
+
+public class Client {
+    public static void main (String[] args) {
+        String hostAddress;
+        int tcpPort;
+        int udpPort;
+    
+        if (args.length != 3) {
+        System.out.println("ERROR: Provide 3 arguments");
+        System.out.println("\t(1) <hostAddress>: the address of the server");
+        System.out.println("\t(2) <tcpPort>: the port number for TCP connection");
+        System.out.println("\t(3) <udpPort>: the port number for UDP connection");
+        System.exit(-1);
+        }
+    
+        hostAddress = args[0];
+        tcpPort = Integer.parseInt(args[1]);
+        udpPort = Integer.parseInt(args[2]);
+    
+        char protocol = 'T';  //default protocol is TCP
+        int bufferLen = 1024;
+            
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNextLine()) {
+            String cmd = sc.nextLine();
+            String[] tokens = cmd.split(" ");
+
+            if (tokens[0].equals("quit")) System.exit(-1);
+            
+            if (tokens[0].equals("setmode")) {
+                switch (tokens[1]) {
+                    case "T": 
+                        protocol =  'T';
+                        System.out.println("Using TCP");
+                        break;
+                    case "U": 
+                        protocol = 'U';
+                        System.out.println("Using UDP");
+                        break;
+                    default: System.out.println("Invalid value");
+                        continue;            
+                }
+            }
+            else{
+                switch(protocol){
+                    case 'T':
+                        try{
+                            //Send message
+                            Socket tcpSocket = new Socket(hostAddress, tcpPort);
+                            PrintWriter outputWriter = new PrintWriter(tcpSocket.getOutputStream(), true);
+                            BufferedReader inputReader = new BufferedReader(new InputStreamReader(tcpSocket.getInputStream()));
+                            outputWriter.write(cmd + "\n");
+                            outputWriter.flush();
+                            
+                            //Receive reply
+                            String recvLine = "";
+                            
+                            while((recvLine = inputReader.readLine()) != null)
+                                    System.out.println(recvLine);
+
+                            tcpSocket.close();
+                        }
+                        catch(IOException ioe){System.err.println(ioe);}
+
+                        break;
+                        
+                    case 'U':
+                        try{
+                            //Send message
+                            byte[] payload = new byte[cmd.length()];
+                            payload = cmd.getBytes();
+                            InetAddress inetAddy = InetAddress.getByName(hostAddress);
+                            DatagramSocket udpOutboundSocket = new DatagramSocket();  //No IP/port for outbound
+                            DatagramSocket udpInboundSocket = new DatagramSocket(udpPort);  //Listen on UDP port for inbound
+                            DatagramPacket sPacket = new DatagramPacket(payload, payload.length, inetAddy, udpPort);
+                            udpOutboundSocket.send(sPacket);
+                            udpOutboundSocket.close();
+                            
+                            //Receive Reply:
+                            byte[] udpBuff = new byte[bufferLen];
+                            DatagramPacket rPacket = new DatagramPacket(udpBuff,udpBuff.length);
+                            udpInboundSocket.receive(rPacket);
+                            String msgData = new String(rPacket.getData());                            
+                            msgData = msgData.trim();
+                            System.out.println(msgData);  
+                            udpInboundSocket.close();
+                        }
+                        catch(SocketException se){System.err.println(se);}
+                        catch(IOException ioe){System.err.println(ioe);}
+                        break;
+}   }   }   }   }
---
+++ Server.java
@@ -0,0 +1,309 @@
+//Distributed Systems Fall 2017
+//Homework 1
+//Patrick Sigourney & Howie Benefiel
+
+import java.util.*;
+import java.io.*;
+import java.net.*;
+import java.lang.Runnable;
+
+class InventoryItem {
+    public String name;
+    public int qty;   
+        
+    public InventoryItem(String nameParam, int qtyParam){
+        name = nameParam;
+        qty = qtyParam;
+    }
+        
+    public void updateQty(int qtyChange){
+        qty -= qtyChange;
+    }
+    
+    public static Comparator<InventoryItem> ItemComparator = new Comparator<InventoryItem>(){
+        public int compare(InventoryItem i1, InventoryItem i2){
+            String item1Name = i1.name.toUpperCase();
+            String item2Name = i2.name.toUpperCase();
+            return item1Name.compareTo(item2Name);
+        }
+    };
+}
+
+class Order {
+    public int id;
+    public String username;
+    public String productName;
+    public int qty;
+    
+    public Order(int idParam, String usernameParam, String productNameParam, int qtyParam){
+        id = idParam;
+        username = usernameParam;
+        productName = productNameParam;
+        qty = qtyParam;
+    }
+}
+
+
+class Listener implements Runnable{
+    public String protocol;
+    public int tcpPort;
+    public int udpPort;
+    
+    public Listener(String protocolParam, int portParam){
+        protocol = protocolParam;
+        if(protocolParam == "U") udpPort = portParam;
+        if(protocolParam == "T") tcpPort = portParam;
+    }
+    
+    public void run(){
+        try{
+            switch(protocol){
+                case "U":
+                    DatagramSocket udpSocket = new DatagramSocket(udpPort);
+                    DatagramPacket recvPacket;
+                    int bufferLen = 1024;
+                    byte[] udpBuff;
+                    
+                    while(true){
+                        udpBuff = new byte[bufferLen];
+                        recvPacket = new DatagramPacket(udpBuff,udpBuff.length);
+                        udpSocket.receive(recvPacket);
+                        
+                        //Spawn off a UDP ServerThread instance to process and reply to the received packet
+                        Runnable udpServerThread = new ServerThread(recvPacket, udpPort);
+                        new Thread(udpServerThread).start();
+                    }
+                    
+                case "T":
+                    ServerSocket tcpServerSocket = new ServerSocket(tcpPort);
+                    Socket tcpClientSocket;
+                    
+                    while(true){
+                        tcpClientSocket = tcpServerSocket.accept();
+                        
+                        //Spawn off a TCP ServerThread instance to read the buffer from the connection which was just accepted
+                        Runnable tcpServerThread = new ServerThread(tcpClientSocket, tcpPort);
+                        new Thread(tcpServerThread).start();
+                    }
+            }
+        }
+        catch(SocketException se){System.err.println(se);}
+        catch(IOException ioe){System.err.println(ioe);}
+    }
+    
+}
+
+
+class ServerThread implements Runnable{
+    public static ArrayList<InventoryItem> invList = new ArrayList<InventoryItem>();
+    public static ArrayList<Order> orderList = new ArrayList<Order>();
+    public static int maxOrderID = 100;  //Initialize the orderIDs
+    
+    public String protocol;  //T or U
+    public int port;
+    
+    public Socket tcpClientSocket;
+    public DatagramPacket udpRecvPacket;
+    
+    public ServerThread(Socket tcpClientSocketParam, int tcpPort){
+       //Constructor for incoming TCP request 
+       protocol = "T";
+	   port = tcpPort;
+       tcpClientSocket = tcpClientSocketParam;
+	}
+
+    public ServerThread(DatagramPacket udpRecvPacketParam, int udpPort){
+        //Constructor for incoming UDP datagram
+        protocol = "U";
+		port = udpPort;
+        udpRecvPacket = udpRecvPacketParam;
+    }
+    
+    
+    
+    public static String processMessage(String[] msgArray){
+        String replyMessage = "";
+        switch(msgArray[0]){
+            case "purchase":
+                return replyMessage = purchase(msgArray);
+        
+             case "cancel":
+                return replyMessage = cancel(msgArray);
+                
+            case "search":
+                return replyMessage = search(msgArray);
+                
+            case "list":
+                return replyMessage = list();
+        }
+        return "Invalid request";
+    }
+    
+    public static String purchase(String[] msgArray){
+        int foundProd = 0;
+        String replyMessage = "";
+        if(msgArray.length == 4) {
+            for(InventoryItem myItem : invList){
+                if(myItem.name != null && myItem.name.equals(msgArray[2])){
+                    foundProd = 1;
+                    if(myItem.qty >= Integer.parseInt(msgArray[3])){
+                        maxOrderID += 1;
+                        Order myOrder = new Order(maxOrderID, msgArray[1], msgArray[2], Integer.parseInt(msgArray[3]));
+                        orderList.add(myOrder);
+                        myItem.updateQty(Integer.parseInt(msgArray[3]));
+                        return replyMessage = "Your order has been placed, " + maxOrderID + ", " + msgArray[2] + ", " + msgArray[3];
+                    }
+                    else return replyMessage = "Not Available - Not enough items";
+                }
+            }
+            if(foundProd == 0) return replyMessage = "Not Available - We do not sell this product";
+        }
+        else return replyMessage = "Incorrect number of parameters for PURCHASE";
+        return replyMessage;
+    };
+    
+    public static String cancel(String[] msgArray){
+        int foundOrder = 0;
+        String replyMessage = "";
+        if(msgArray[1].matches("^\\d+$")){
+            for(Order myOrder : orderList){
+                if(myOrder.id == Integer.parseInt(msgArray[1])){
+                    foundOrder = 1;
+                    for(InventoryItem myItem : invList){
+                        if(myItem.name.equals(myOrder.productName)){
+                            myItem.updateQty(-1 * myOrder.qty);
+                        }
+                    }
+                    orderList.remove(orderList.indexOf(myOrder));
+                    return replyMessage = "Order " + msgArray[1] + " is cancelled";
+                }
+            }
+            if(foundOrder == 0) return replyMessage = msgArray[1] + " not found, no such order";
+        }
+        else return replyMessage = "Order ID is not a number";
+        return replyMessage;
+    };
+
+    public static String search(String[] msgArray){
+        int foundOrder = 0;
+        String replyMessage = "";
+        for(Order myOrder : orderList){
+            if(myOrder.username.equals(msgArray[1])){
+                foundOrder = 1;
+                replyMessage += myOrder.id + ", " + myOrder.productName + ", " + myOrder.qty + "\n";
+            }
+        }
+        if(foundOrder == 0) return replyMessage = "No order found for " + msgArray[1];
+        return replyMessage;
+    };
+
+    public static String list(){
+        String replyMessage = "";
+        for(InventoryItem myItem : invList)
+            replyMessage += myItem.name + " " + myItem.qty + "\n";
+
+        return replyMessage;
+    };
+
+    
+    public void run(){
+		
+        try{
+			String[] msgArray;
+            switch(protocol){
+                case "U":
+                    //We have received a UDP DatagramPacket; process it and reply back
+                
+                    //Pull out message and sender IP
+                    InetAddress msgAddr = udpRecvPacket.getAddress();
+                    String msgData = new String(udpRecvPacket.getData());
+                    msgArray = msgData.trim().split("\\s+");
+					
+					DatagramSocket udpOutboundSocket = new DatagramSocket();
+
+                    //Process the message
+                    String replyMessage = processMessage(msgArray);
+				                    
+                    //Generate reply
+                    byte[] payload = new byte[replyMessage.length()];
+                    payload = replyMessage.getBytes();
+                    DatagramPacket sendPacket = new DatagramPacket(payload, payload.length, msgAddr, port);
+					
+                    //Send reply
+                    udpOutboundSocket.send(sendPacket);
+					udpOutboundSocket.close();
+					break;
+                    
+                case "T":
+                    //We have received a TCP socket from the client.  Receive message and reply.
+					
+                    BufferedReader inputReader = new BufferedReader(new InputStreamReader(tcpClientSocket.getInputStream()));
+                    PrintWriter outputWriter = new PrintWriter(tcpClientSocket.getOutputStream(), true);
+					
+					String inputLine = inputReader.readLine();
+					
+					if(inputLine.length() > 0){
+						msgArray = inputLine.trim().split("\\s+");
+						
+						replyMessage = processMessage(msgArray);
+					
+						String[] replyArray = replyMessage.split(System.getProperty("line.separator"));
+
+						for(int x=0; x < replyArray.length; x++)
+							outputWriter.write(replyArray[x]); 
+
+						outputWriter.flush();
+						outputWriter.close();
+					}
+					break;
+            }
+        }
+        catch(SocketException se){System.err.println(se);}
+        catch(IOException ioe){System.err.println(ioe);}
+    }
+}
+    
+    
+    
+public class Server {
+    public static void main (String[] args) throws IOException {
+        int tcpPort;
+        int udpPort;
+        if (args.length != 3) {
+            System.out.println("ERROR: Provide 3 arguments");
+            System.out.println("\t(1) <tcpPort>: the port number for TCP connection");
+            System.out.println("\t(2) <udpPort>: the port number for UDP connection");
+            System.out.println("\t(3) <file>: the file of inventory");
+        
+            System.exit(-1);
+        }
+
+        tcpPort = Integer.parseInt(args[0]);
+        udpPort = Integer.parseInt(args[1]);
+        String fileName = args[2];
+    
+        // parse and sort the inventory file
+        FileReader invFile = new FileReader(fileName);
+        BufferedReader invBuff = new BufferedReader(invFile);
+        String invLine = null;
+        while((invLine = invBuff.readLine()) != null){
+            if(invLine.length() > 0){
+                String[] itemArr = invLine.split("\\s+");
+                if(itemArr.length == 2){
+                    InventoryItem newItem = new InventoryItem(itemArr[0], Integer.parseInt(itemArr[1]));
+                    ServerThread.invList.add(newItem);
+        }   }   }
+        Collections.sort(ServerThread.invList, InventoryItem.ItemComparator);  //Sort the inventory items by name
+        
+        
+        
+        //spawn tcpListener;
+        Runnable tr = new Listener("T", tcpPort);
+        new Thread(tr).start();
+        
+        //spawn udpListener;
+        Runnable ur = new Listener("U", udpPort);
+        new Thread(ur).start();
+        
+    }
+}
