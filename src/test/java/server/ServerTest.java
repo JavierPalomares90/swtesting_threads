30fe5e1c94b5999eb9aa5680b644d3ff65e3641d
Patrick Sigourney <psigourney@users.noreply.github.com>
2018-07-16 15:57:30 -0500

    Removed UDP port parameter

--- src/test/java/server/ServerTest.java
+++ src/test/java/server/ServerTest.java
@@ -9,7 +9,7 @@ public class ServerTest
     @Test
     public void testMain() throws IOException
     {
-        String[] inputs = new String[]{"1234", "1234", "src/test/resources/inventory.txt"};
+        String[] inputs = new String[]{"1234", "src/test/resources/inventory.txt"};
         Server.main(inputs);
         /** TODO: Complete testing **/
     }
